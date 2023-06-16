3f326d6ba8c63780604b0398aff3b53f6688428d
Tine Kleivane <kleivane@gmail.com>
2023-06-16 15:27:16 +0200

    Mockserver-oppdatering

--- maskinporten-mock/routes/datasharing.js
+++ maskinporten-mock/routes/datasharing.js
@@ -1,17 +1,41 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+// FOR KONSUMENTER:
+
+// For å registrere ny maskinporten-integrasjon
 router.post('/scope/client', function (req, res, next) {
+
+    /**
+     * BODY:
+     * {
+     *   "description": "Description for client",
+     *   "scopes": [ "scope1", "scope2" ]
+     * }
+     * */
+
     var client = {
         "client_id": "64067a31-e9a1-48c2-b2e1-989255eaf864",
         "consumer_orgno": "00998877",
         "description": "Description for client",
         "scopes": ["scope1", "scope2"]
     }
+
+    /**
+     * Respons ved feila registrering (f.eks. ved manglende tilgang til scope):
+     *
+     * HTTP/1.1 401 FORBIDDEN
+     * Content-Type: application/json
+     *
+     * {
+     *   "error_id": "invalid_scope",
+     *   "error_description": "scope <scope1> not found or client organization is not granted access to scope"
+     * }
+     */
     res.send(client);
 });
 
+// For å hente ut alle maskinporten-integrasjoner tilhøyrende innlogga bruker sin organsiasjon:
 router.get('/client', function (req, res, next) {
     var clients = [{
         "client_id": "64067a31-e9a1-48c2-b2e1-989255eaf864",
@@ -27,21 +51,7 @@ router.get('/client', function (req, res, next) {
     res.send(clients);
 });
 
-// For å registrere tilgang til scope for ny konsument:
-router.put('/scope/access', function (req, res, next) {
-    /**
-     * BODY:
-     * {
-     *   "consumer_orgno": "<orgno>",
-     *   "scope": "<scope>"
-     * }
-     * */
-
-
-    // scope ikkje tilhører innlogga bruker sin organisasjon, eller bruker mangler riktig tilgang i Altinn: HTTP/1.1 401 FORBIDDEN
-    res.send();
-})
-
+// lista opp alle scopes som organisasjonen har fått tilgang til
 router.get('/scope/access', function (req, res, next) {
     var scope = [{
         "name": "pilot:api",
@@ -58,7 +68,27 @@ router.get('/scope/access', function (req, res, next) {
     }
     ]
     res.send(scope);
-})
-    ;
+});
+
+// FOR TILBYDER:
+//
+// For å registrere tilgang til scope for ny konsument:
+router.put('/scope/access', function (req, res, next) {
+    /**
+     * BODY:
+     * {
+     *   "consumer_orgno": "<orgno>",
+     *   "scope": "<scope>"
+     * }
+     * */
+
+
+    // scope ikkje tilhører innlogga bruker sin organisasjon, eller bruker mangler riktig tilgang i Altinn: HTTP/1.1 401 FORBIDDEN
+    res.send();
+});
+
+// GET /datasharing/scope
+// kun diskutert litt - meir naturleg lista opp scopes som organisasjonen EIG sjølv
+
 
     module.exports = router;
