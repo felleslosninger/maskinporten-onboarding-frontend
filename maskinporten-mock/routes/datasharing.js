4f73d0d595ddce0f1c8dcf79f8f5e16171c17d56
Tine Kleivane <kleivane@gmail.com>
2023-06-16 15:22:51 +0200

    Mockserver-oppdatering

--- maskinporten-mock/routes/datasharing.js
+++ maskinporten-mock/routes/datasharing.js
@@ -2,34 +2,63 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.post('/scope/client', function(req, res, next) {
-  var client = {
-    "client_id": "64067a31-e9a1-48c2-b2e1-989255eaf864",
-      "consumer_orgno": "00998877",
-    "description": "Description for client",
-      "scopes": [ "scope1", "scope2" ]
-  }
-  res.send(client);
+router.post('/scope/client', function (req, res, next) {
+    var client = {
+        "client_id": "64067a31-e9a1-48c2-b2e1-989255eaf864",
+        "consumer_orgno": "00998877",
+        "description": "Description for client",
+        "scopes": ["scope1", "scope2"]
+    }
+    res.send(client);
 });
 
-router.get('/client', function(req, res, next) {
-  var clients = [{
-    "client_id": "64067a31-e9a1-48c2-b2e1-989255eaf864",
-    "consumer_orgno": "00998877",
-    "description": "Description for client",
-    "scopes": [ "scope1", "scope2" ]
-  }, {
-    "client_id": "uuid",
-    "consumer_orgno": "00998877",
-    "description": "Description for second client",
-    "scopes": [ "scope1" ]
-  }]
-  res.send(clients);
+router.get('/client', function (req, res, next) {
+    var clients = [{
+        "client_id": "64067a31-e9a1-48c2-b2e1-989255eaf864",
+        "consumer_orgno": "00998877",
+        "description": "Description for client",
+        "scopes": ["scope1", "scope2"]
+    }, {
+        "client_id": "uuid",
+        "consumer_orgno": "00998877",
+        "description": "Description for second client",
+        "scopes": ["scope1"]
+    }]
+    res.send(clients);
 });
 
-router.get('/scope/access', function(req, res, next) {
-  var scope = ["scope1", "scope2" ]
-  res.send(scope);
-});
+// For å registrere tilgang til scope for ny konsument:
+router.put('/scope/access', function (req, res, next) {
+    /**
+     * BODY:
+     * {
+     *   "consumer_orgno": "<orgno>",
+     *   "scope": "<scope>"
+     * }
+     * */
 
-module.exports = router;
+
+    // scope ikkje tilhører innlogga bruker sin organisasjon, eller bruker mangler riktig tilgang i Altinn: HTTP/1.1 401 FORBIDDEN
+    res.send();
+})
+
+router.get('/scope/access', function (req, res, next) {
+    var scope = [{
+        "name": "pilot:api",
+        "active": true,
+        "prefix": "pilot",
+        "subscope": "api",
+        "description": "Gir tilgang til et eksempelapi for å støttet tines mockapI"
+        }, {
+        "name": "pilot:datasett",
+        "active": true,
+        "prefix": "pilot",
+        "subscope": "datasett",
+        "description": "Gir tilgang til et eksempel-skyportenressurs for å støttet tines mockapI"
+    }
+    ]
+    res.send(scope);
+})
+    ;
+
+    module.exports = router;
