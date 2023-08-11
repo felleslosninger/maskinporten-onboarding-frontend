6ee6e3c6bf677e367a9faf19d6b47d4eeeaa7ed6
Tine Kleivane <kleivane@gmail.com>
2023-08-11 15:54:31 +0200

    Mye mer beskrivelse på fremgangsmåte for å generere token

--- src/components/onboarding/TabsSection/TabsSection.tsx
+++ src/components/onboarding/TabsSection/TabsSection.tsx
@@ -9,8 +9,6 @@ import {TasklistIcon, KeyHorizontalIcon, BagdeIcon} from "@navikt/aksel-icons";
 import sertificateCode from "../CodeExample/sertificateCode.json";
 import keyCode from "../CodeExample/keyCode.json";
 
-
-
 function TabsSection() {
     const [selectedTab, setSelectedTab] = useState(1);
 
@@ -29,24 +27,51 @@ function TabsSection() {
                 {bold("Forutsetninger:")}
                 <ol className={`${styles.paragraph} ${styles.orderedList}`}>
                     <li>
-                        [TEST-]virksomhetssertifikat. {link("/", "Hvordan skaffer jeg et virksomhetssertifikat?")}
+                        Det finnes et virksomhetssertifikat tilgjengelig for rett miljø. {link("https://docs.digdir.no/docs/Maskinporten/maskinporten_autentisering#hvem-kan-utstede-virksomhetssertifikater", "Hvordan skaffer jeg et virksomhetssertifikat?")}
                     </li>
                     <li>
-                        Opprettet ny klient eller bruk valgt klient.
+                        API-tilgangen er gitt til din virksomhet av organisasjonen som tilbyr API'et. Når dette er gjort vil du se tilgangen i {link("/dashboard", "oversikten")}.
+                    </li>
+                    <li>
+                        Det er opprett en {link("/dashboard", "ny klient ")} eller en eksisterende klient som du ønsker å gjenbruke
                     </li>
                 </ol>
                 <Heading size={"medium"}>
                     Fremgangsmåte
                 </Heading>
                 <ol className={styles.orderedList}>
-                    <li>Lag JWT-grant som inneholder det fulle virksomhetssertifikatet i headeren.</li>
-                    <li>Signer JWT-grant med virksomhetssertifikatet.</li>
-                    <li>Send til maskinporten.</li>
-                    <li>Bruk det returnerte tokenet som bearer-token mot tjenesten.</li>
+                    <li>Lag JWT-grant i henhold til {link("https://docs.digdir.no/docs/Maskinporten/maskinporten_protocol_jwtgrant","standarden")}. Følgende felter er påkrevd</li>
+                        <ol>
+                            <li>Headerfeltet <code>alg</code>: RS256, RS384 eller RS512 er mulige verdier støttet av Maskinporten</li>
+                            <li>Headerfeltet <code>x5c</code>: Virksomhetssertifikat </li>
+                            <li>Bodyfeltet <code>aud</code>: __MASKINPORTEN_URL__</li>
+                            <li>Bodyfeltet <code>iss</code>: client-id for klienten du ønsker å bruke</li>
+                            <li>Bodyfeltet <code>scope</code>: scopet knyttet til apiet du vil aksessere</li>
+                            <li>Bodyfeltet <code>ita</code>: Timestamp i UTC-tid</li>
+                            <li>Bodyfeltet <code>exp</code>: Timestamp for utgått tilgang i UTC-tid</li>
+                        </ol>
+                    Anbefalt eller knyttet til APIet som skal benyttes
+                    <ol>
+                        <li>Bodyfeltet <code>jti</code>: unik uuid</li>
+                        <li>Bodyfeltet <code>resource</code>: Dersom API-tilbyder har spesifisert en audience-begrensning på sitt API må den settes her.
+                            Gyldige verdier må beskrives av API-tilbyder. Se mer om {link("https://docs.digdir.no/docs/Maskinporten/maskinporten_func_audience_restricted_tokens", "audience-begrensning", true)}</li>
+                    </ol>
+                    <li>Signer JWT-grant med virksomhetssertifikatet</li>
+                    <li>POST med content-type <code>application/x-www-form-urlencoded</code> til Maskinporten sin issuer-url __MASKINPORTEN_URL__
+                        <ol>
+                            <li>Parameteret <code>grant</code> med verdi <code>urn:ietf:params:oauth:grant-type:jwt-bearer</code></li>
+                            <li>Parameteret <code>assertion</code> med serialisert JWT-grant som verdi</li>
+                        </ol>
+                    </li>
+                    <li>I POST-responsen vil du motta et JSON. Feltet <code>access_token</code> vil være autentisering
+                        som trengs mot tjenestetilbyder. <code>expiresIn</code> vil beskrive hvor lenge tokenet er gyldig,
+                        vanligvis 2 minutter.
+                    </li>
+                    <li>Gjør en kall til API-tjenesten med innholdet i <code>access_token</code> som en del av http-headeren </li>
                 </ol>
             </InfoBox>
 
-            <CodeExample title={"Se kode-eksempel JWT-grant"}>
+            <CodeExample title={"Eksempelkode"}>
                 <CodeLanguage title={"Java"} language={"java"} code={sertificateCode.java}/>
             </CodeExample>
         </>
