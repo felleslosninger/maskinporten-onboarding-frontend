96f456fc195d8b1df83fdf28987126ed73c871de
Tine Kleivane <kleivane@gmail.com>
2023-08-24 12:46:36 +0200

    Forbedre fremgangsmåte med nøkkel

--- src/components/onboarding/TabsSection/TabsSection.tsx
+++ src/components/onboarding/TabsSection/TabsSection.tsx
@@ -58,7 +58,7 @@ function TabsSection() {
                             Gyldige verdier må beskrives av API-tilbyder. Se mer om {link("https://docs.digdir.no/docs/Maskinporten/maskinporten_func_audience_restricted_tokens", "audience-begrensning", true)}</li>
                     </ol>
                     <li>Signer JWT-grant med virksomhetssertifikatet</li>
-                    <li>POST med content-type <code>application/x-www-form-urlencoded</code> til Maskinporten sitt token-endepunkt* og følgende queryparametre
+                    <li>POST med content-type <code>application/x-www-form-urlencoded</code> til Maskinporten sitt token-endepunkt* og følgende parametre i body
                         <ol>
                             <li><code>grant</code> med verdi <code>urn:ietf:params:oauth:grant-type:jwt-bearer</code></li>
                             <li><code>assertion</code> med serialisert JWT-grant som verdi</li>
@@ -68,7 +68,7 @@ function TabsSection() {
                         som trengs mot tjenestetilbyder. <code>expiresIn</code> vil beskrive hvor lenge tokenet er gyldig,
                         vanligvis 2 minutter.
                     </li>
-                    <li>Gjør en kall til API-tjenesten med innholdet i <code>access_token</code> som en del av http-headeren </li>
+                    <li>Gjør en kall til API-tjenesten med innholdet i <code>access_token</code> som en del av http-headeren: <code>Authentication: Bearer accesstoken-verdi</code> </li>
                 </ol>
 
                 * Maskinporten har følgende urler i  miljøene
@@ -86,29 +86,74 @@ function TabsSection() {
     const keySection = () => (
         <>
             <Heading size={"large"} className={styles.largeHeader}>
-                Med manuelt opplastede nøkler
+                Med manuelt opplastet nøkkel
             </Heading>
             <Paragraph className={styles.paragraph}>
-                Lag eget nøkkelpar og last opp til klienten. Referer til
-                nøkkelen i tokenet of signer med rett sertifikat.
+                Her lager du ditt eget asymmentriske nøkkelpar. Når du oppretter en ny integrasjon, legger du
+                ved public-nøkkelen du ønsker å bruke. Du får en key id (kid) som du kan referer til som en del
+                av JWT-grant og bruker privatnøkkelen til å signere hele JWT-tokenet
             </Paragraph>
 
             <InfoBox>
                 {bold("Forutsetninger:")}
-                <Paragraph className={styles.paragraph}>
-                    Nøkkelen må lastes opp samtidig med at klienten opprettes.
-                </Paragraph>
+                <ol className={`${styles.paragraph} ${styles.orderedList}`}>
+
+                    <li>
+                        API-tilgangen er gitt til din virksomhet av organisasjonen som tilbyr API'et. Når dette er gjort
+                        vil du se tilgangen i {link("/dashboard", "oversikten")}.
+                    </li>
+                    <li>
+                        Du har opprettet et asymmentrisk nøkkelpar av typen RSA256 og har public-delen av nøkkelen tilgjengelig.
+                        Om du ikke har et eksisterende nøkkelpar kan du opprette et med kommandoene
+                        <ol>
+                            <li><code>openssl genrsa -out maskinporten.pem 2048</code></li>
+                            <li><code>openssl rsa -in maskinporten.pem -pubout -out maskinporten.pub</code>henter ut
+                                public-delen til fila maskinporten.pub </li>
+                        </ol>
+                    </li>
+                    <li>
+                        Det er opprett en {link("/dashboard", "ny klient ")} eller en eksisterende klient som du
+                        ønsker å gjenbruke et eksisterende nøkkelsett. Dersom du oppretter ny klient, last opp public-delen av
+                        nøkkelen du ønsker å bruke ved opprettelse.
+                    </li>
+                </ol>
                 <Heading size={"medium"}>
                     Fremgangsmåte
                 </Heading>
                 <ol className={styles.orderedList}>
-                    <li>Lag klient med med nøkkelpar fra for eksempel {link("https://mkjwk.org", "mkjwk.org", true)}.
+                    <li>Lag JWT-grant i henhold til {link("https://docs.digdir.no/docs/Maskinporten/maskinporten_protocol_jwtgrant","standarden")}. Følgende felter er påkrevd</li>
+                    <ol>
+                        <li>Headerfeltet <code>alg</code>: RS256, RS384 eller RS512 er mulige verdier støttet av Maskinporten</li>
+                        <li>Headerfeltet <code>kid</code>: key-id (kid) fra integrasjonen du ønsker å bruke</li>
+                        <li>Bodyfeltet <code>aud</code>: Maskinporten-url*</li>
+                        <li>Bodyfeltet <code>iss</code>: client-id for integrasjonen du ønsker å bruke</li>
+                        <li>Bodyfeltet <code>scope</code>: scopet knyttet til apiet du vil aksessere</li>
+                        <li>Bodyfeltet <code>ita</code>: Timestamp i UTC-tid</li>
+                        <li>Bodyfeltet <code>exp</code>: Timestamp for utgått tilgang i UTC-tid</li>
+                    </ol>
+                    Anbefalt eller knyttet til APIet som skal benyttes
+                    <ol>
+                        <li>Bodyfeltet <code>jti</code>: unik uuid</li>
+                        <li>Bodyfeltet <code>resource</code>: Dersom API-tilbyder har spesifisert en audience-begrensning på sitt API må den settes her.
+                            Gyldige verdier må beskrives av API-tilbyder. Se mer om {link("https://docs.digdir.no/docs/Maskinporten/maskinporten_func_audience_restricted_tokens", "audience-begrensning", true)}</li>
+                    </ol>
+                    <li>Signer JWT-grant med privatnøkkelen du har opprettet</li>
+                    <li>POST med content-type <code>application/x-www-form-urlencoded</code> til Maskinporten sitt token-endepunkt* og følgende parametre i body
+                        <ol>
+                            <li><code>grant</code> med verdi <code>urn:ietf:params:oauth:grant-type:jwt-bearer</code></li>
+                            <li><code>assertion</code> med serialisert JWT-grant som verdi</li>
+                        </ol>
                     </li>
-                    <li>Lag JWT-grant og referer til kid.</li>
-                    <li>Signer med privatnøkkelen opprettet i steg 1.</li>
-                    <li>Send til maskinporten.</li>
-                    <li>Bruk det returnerte tokenet som bearer-token mot tjenesten.</li>
+                    <li>I POST-responsen vil du motta et JSON. Feltet <code>access_token</code> vil være autentisering
+                        som trengs mot tjenestetilbyder. <code>expiresIn</code> vil beskrive hvor lenge tokenet er gyldig,
+                        vanligvis 2 minutter.
+                    </li>
+                    <li>Gjør en kall til API-tjenesten med innholdet i <code>access_token</code> som en del av http-headeren: <code>Authentication: Bearer accesstoken-verdi</code> </li>
                 </ol>
+
+                * Maskinporten har følgende urler i  miljøene
+                /config-info
+
             </InfoBox>
 
             <CodeExample title={"Se kode-eksempel JWT-grant"}>
@@ -166,7 +211,7 @@ function TabsSection() {
                 <div className={`${styles.tab} ${selectedTab === 2 ? styles.active : styles.inactive}`}
                      onClick={() => setSelectedTab(2)}>
                     <KeyHorizontalIcon/>
-                    <Label size={"large"}>Manuelt opplastede nøkler</Label>
+                    <Label size={"large"}>Manuelt opplastet nøkkel</Label>
                 </div>
                 <div className={`${styles.tab} ${selectedTab === 3 ? styles.active : styles.inactive}`}
                      onClick={() => setSelectedTab(3)}>
