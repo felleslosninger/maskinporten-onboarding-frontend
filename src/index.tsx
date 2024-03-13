87b72dcefa5fd94cbc847d1daac0def48badb1fc
Lars Kåre Syversen <lars@NO-WJXMWCC6XF.local>
2024-03-13 13:41:36 +0100

    sender nå flere requests jevlig for å holde connection mot datasharing api

--- src/index.tsx
+++ src/index.tsx
@@ -24,9 +24,7 @@ const root = ReactDOM.createRoot(
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      retry: 0,
-      refetchOnMount: false,
-      refetchOnWindowFocus: false,
+      retry: 0
     },
   },
 });
